6f31dfd25595006e6f86850232de5f7cf1149cc0
Somoy73 <somoy97@gmail.com>
2024-05-21 15:37:01 +0300

    add planner build path logic

--- ai_control.py
+++ ai_control.py
@@ -8,10 +8,16 @@ import math
 import numpy as np
 
 try:
-    sys.path.append(glob.glob('**/*%d.%d-%s.egg' % (
-        sys.version_info.major,
-        sys.version_info.minor,
-        'win-amd64' if os.name == 'nt' else 'linux-x86_64'))[0])
+    sys.path.append(
+        glob.glob(
+            "**/*%d.%d-%s.egg"
+            % (
+                sys.version_info.major,
+                sys.version_info.minor,
+                "win-amd64" if os.name == "nt" else "linux-x86_64",
+            )
+        )[0]
+    )
 except IndexError:
     pass
 
@@ -19,126 +25,154 @@ import carla
 import ai_knowledge as data
 from ai_knowledge import Status
 
+
 # Executor is responsible for moving the vehicle around
 # In this implementation it only needs to match the steering and speed so that we arrive at provided waypoints
 # BONUS TODO: implement different speed limits so that planner would also provide speed target speed in addition to direction
 class Executor(object):
-  def __init__(self, knowledge, vehicle):
-    self.vehicle = vehicle
-    self.knowledge = knowledge
-    self.target_pos = knowledge.get_location()
-    
-  #Update the executor at some intervals to steer the car in desired direction
-  def update(self, time_elapsed):
-    status = self.knowledge.get_status()
-    #TODO: this needs to be able to handle
-    if status == Status.DRIVING:
-      dest = self.knowledge.get_current_destination()
-      self.update_control(dest, [1], time_elapsed)
+    def __init__(self, knowledge, vehicle):
+        self.vehicle = vehicle
+        self.knowledge = knowledge
+        self.target_pos = knowledge.get_location()
 
-  # TODO: steer in the direction of destination and throttle or brake depending on how close we are to destination
-  # TODO: Take into account that exiting the crash site could also be done in reverse, so there might need to be additional data passed between planner and executor, or there needs to be some way to tell this that it is ok to drive in reverse during HEALING and CRASHED states. An example is additional_vars, that could be a list with parameters that can tell us which things we can do (for example going in reverse)
-  def update_control(self, destination, additional_vars, delta_time):
-    
-    #calculate throttle and heading
-    target_speed = additional_vars[0] if additional_vars else 1.0
-    # Get vehicle's current transform and location
-    vehicle_transform = self.vehicle.get_transform()
-    vehicle_location = vehicle_transform.location
-    vehicle_rotation = vehicle_transform.rotation
+    # Update the executor at some intervals to steer the car in desired direction
+    def update(self, time_elapsed):
+        status = self.knowledge.get_status()
+        # TODO: this needs to be able to handle
+        if status == Status.DRIVING:
+            dest = self.knowledge.get_current_destination()
+            self.update_control(dest, [1], time_elapsed)
 
-    # Convert vehicle's current location and destination into numpy arrays
-    vehicle_pos = np.array([vehicle_location.x, vehicle_location.y])
-    destination_pos = np.array([destination.x, destination.y])
+    # TODO: steer in the direction of destination and throttle or brake depending on how close we are to destination
+    # TODO: Take into account that exiting the crash site could also be done in reverse, so there might need to be additional data passed between planner and executor, or there needs to be some way to tell this that it is ok to drive in reverse during HEALING and CRASHED states. An example is additional_vars, that could be a list with parameters that can tell us which things we can do (for example going in reverse)
+    def update_control(self, destination, additional_vars, delta_time):
 
-    # Calculate the vector from the vehicle to the destination
-    vector_to_destination = destination_pos - vehicle_pos
-    vector_to_destination_normalized = vector_to_destination / np.linalg.norm(vector_to_destination)
+        # calculate throttle and heading
+        target_speed = additional_vars[0] if additional_vars else 1.0
+        # Get vehicle's current transform and location
+        vehicle_transform = self.vehicle.get_transform()
+        vehicle_location = vehicle_transform.location
+        vehicle_rotation = vehicle_transform.rotation
 
-    # Get vehicle's forward vector
-    forward_vector = np.array([np.cos(np.radians(vehicle_rotation.yaw)), np.sin(np.radians(vehicle_rotation.yaw))])
+        # Convert vehicle's current location and destination into numpy arrays
+        vehicle_pos = np.array([vehicle_location.x, vehicle_location.y])
+        destination_pos = np.array([destination.x, destination.y])
 
-    # Dot product and cross product to find the angle to the destination
-    dot_product = np.dot(forward_vector, vector_to_destination_normalized)
-    cross_product = np.cross(forward_vector, vector_to_destination_normalized)
+        # Calculate the vector from the vehicle to the destination
+        vector_to_destination = destination_pos - vehicle_pos
+        vector_to_destination_normalized = vector_to_destination / np.linalg.norm(
+            vector_to_destination
+        )
 
-    # Calculate steering angle (angle between vehicle's forward direction and destination direction)
-    angle_to_destination = np.arccos(np.clip(dot_product, -1.0, 1.0))
-    steer_direction = np.sign(cross_product)
+        # Get vehicle's forward vector
+        forward_vector = np.array(
+            [
+                np.cos(np.radians(vehicle_rotation.yaw)),
+                np.sin(np.radians(vehicle_rotation.yaw)),
+            ]
+        )
 
-    # Create vehicle control object
-    control = carla.VehicleControl()
-    control.throttle = 0.5  # You might want to adjust this based on distance to destination and current speed
-    control.steer = steer_direction * (angle_to_destination / np.pi)  # Normalize steering angle to [-1, 1]
-    control.brake = 0.0
-    control.hand_brake = False
+        # Dot product and cross product to find the angle to the destination
+        dot_product = np.dot(forward_vector, vector_to_destination_normalized)
+        cross_product = np.cross(forward_vector, vector_to_destination_normalized)
 
-    # Apply the control to the vehicle
-    self.vehicle.apply_control(control)
+        # Calculate steering angle (angle between vehicle's forward direction and destination direction)
+        angle_to_destination = np.arccos(np.clip(dot_product, -1.0, 1.0))
+        steer_direction = np.sign(cross_product)
+
+        # Create vehicle control object
+        control = carla.VehicleControl()
+        control.throttle = 0.5  # You might want to adjust this based on distance to destination and current speed
+        control.steer = steer_direction * (
+            angle_to_destination / np.pi
+        )  # Normalize steering angle to [-1, 1]
+        control.brake = 0.0
+        control.hand_brake = False
+
+        # Apply the control to the vehicle
+        self.vehicle.apply_control(control)
+
 
 # Planner is responsible for creating a plan for moving around
 # In our case it creates a list of waypoints to follow so that vehicle arrives at destination
 # Alternatively this can also provide a list of waypoints to try avoid crashing or 'uncrash' itself
 class Planner(object):
-  def __init__(self, knowledge):
-    self.knowledge = knowledge
-    self.path = deque([])
+    def __init__(self, knowledge, vehicle):
+        self.knowledge = knowledge
+        self.vehicle = vehicle
+        self.path = deque([])
 
-  # Create a map of waypoints to follow to the destination and save it
-  def make_plan(self, source, destination):
-    self.path = self.build_path(source,destination)
-    self.update_plan()
-    self.knowledge.update_destination(self.get_current_destination())
-  
-  # Function that is called at time intervals to update ai-state
-  def update(self, time_elapsed):
-    self.update_plan()
-    self.knowledge.update_destination(self.get_current_destination())
-    print('Planner update called')
-    print('Current Status: ', self.knowledge.get_status())
-    obstacles = self.knowledge.get_obstacles()
-    if obstacles is None:
-      obstacles = []
-    
-    print('Obstacles: ',len(obstacles) )
-  
-  #Update internal state to make sure that there are waypoints to follow and that we have not arrived yet
-  def update_plan(self):
-    if len(self.path) == 0:
-      return
-    
-    if self.knowledge.arrived_at(self.path[0]):
-      self.path.popleft()
-    
-    if len(self.path) == 0:
-      self.knowledge.update_status(Status.ARRIVED)
-  
+    # Create a map of waypoints to follow to the destination and save it
+    def make_plan(self, source, destination):
+        self.path = self.build_path(source, destination)
+        print("Planner set path: ", self.path)
+        self.update_plan()
+        self.knowledge.update_destination(self.get_current_destination())
 
-  #get current destination 
-  def get_current_destination(self):
-    status = self.knowledge.get_status()
-    #if we are driving, then the current destination is next waypoint
-    if status == Status.DRIVING:
-      #TODO: Take into account traffic lights and other cars
-      return self.path[0]
-    if status == Status.ARRIVED:
-      return self.knowledge.get_location()
-    if status == Status.HEALING:
-      #TODO: Implement crash handling. Probably needs to be done by following waypoint list to exit the crash site.
-      #Afterwards needs to remake the path.
-      return self.knowledge.get_location()
-    if status == Status.CRASHED:
-      #TODO: implement function for crash handling, should provide map of wayoints to move towards to for exiting crash state. 
-      #You should use separate waypoint list for that, to not mess with the original path. 
-      return self.knowledge.get_location()
-    #otherwise destination is same as current position
-    return self.knowledge.get_location()
+    # Function that is called at time intervals to update ai-state
+    def update(self, time_elapsed):
+        self.update_plan()
+        self.knowledge.update_destination(self.get_current_destination())
+        print("Planner update called")
+        print("Current Status: ", self.knowledge.get_status())
+        obstacles = self.knowledge.get_obstacles()
+        if obstacles is None:
+            obstacles = []
 
-  #TODO: Implementation
-  def build_path(self, source, destination):
-    self.path = deque([])
-    self.path.append(destination)
-    #TODO: create path of waypoints from source to destination
-    return self.path
+        print("Obstacles: ", len(obstacles))
 
+    # Update internal state to make sure that there are waypoints to follow and that we have not arrived yet
+    def update_plan(self):
+        if len(self.path) == 0:
+            return
 
+        if self.knowledge.arrived_at(self.path[0]):
+            self.path.popleft()
+
+        if len(self.path) == 0:
+            self.knowledge.update_status(Status.ARRIVED)
+
+    # get current destination
+    def get_current_destination(self):
+        status = self.knowledge.get_status()
+        # if we are driving, then the current destination is next waypoint
+        if status == Status.DRIVING:
+            # TODO: Take into account traffic lights and other cars
+            return self.path[0]
+        if status == Status.ARRIVED:
+            return self.knowledge.get_location()
+        if status == Status.HEALING:
+            # TODO: Implement crash handling. Probably needs to be done by following waypoint list to exit the crash site.
+            # Afterwards needs to remake the path.
+            return self.knowledge.get_location()
+        if status == Status.CRASHED:
+            # TODO: implement function for crash handling, should provide map of wayoints to move towards to for exiting crash state.
+            # You should use separate waypoint list for that, to not mess with the original path.
+            return self.knowledge.get_location()
+        # otherwise destination is same as current position
+        return self.knowledge.get_location()
+
+    # TODO: Implementation
+    # TODO: create path of waypoints from source to destination
+    def build_path(self, source, destination):
+        self.path = deque([])
+        world = self.vehicle.get_world()
+        world_map = world.get_map()
+
+        # Get Waypoints from source to destination using Carla's map API
+        source_waypoint = world_map.get_waypoint(source.location)
+        destination_waypoint = world_map.get_waypoint(destination.location)
+
+        # Generating Waypoint less than 5 meters (may need to change the condition in knowledge)
+        current_waypoint = source_waypoint
+        print("Source of the Car: ", current_waypoint)
+        print("Destination of the Car: ", destination_waypoint)
+        while current_waypoint.transform.location.distance(destination.location) > 2.5:
+            next_waypoint = current_waypoint.next(2.5)[
+                0
+            ]  # Generate waypoints every 2.5 meters
+            self.path.append(next_waypoint.transform.location)
+            current_waypoint = next_waypoint
+
+        self.path.append(destination)
+        return self.path
--- custom_ai.py
+++ custom_ai.py
@@ -5,10 +5,16 @@ import os
 import sys
 
 try:
-    sys.path.append(glob.glob('**/*%d.%d-%s.egg' % (
-        sys.version_info.major,
-        sys.version_info.minor,
-        'win-amd64' if os.name == 'nt' else 'linux-x86_64'))[0])
+    sys.path.append(
+        glob.glob(
+            "**/*%d.%d-%s.egg"
+            % (
+                sys.version_info.major,
+                sys.version_info.minor,
+                "win-amd64" if os.name == "nt" else "linux-x86_64",
+            )
+        )[0]
+    )
 except IndexError:
     pass
 
@@ -18,53 +24,49 @@ import ai_control as control
 import ai_parser as parser
 import time
 
-#Manager Script
+
+# Manager Script
 class Autopilot(object):
-  def __init__(self, vehicle):
-    self.vehicle = vehicle
-    self.knowledge = data.Knowledge()
-    self.knowledge.set_status_changed_callback(self.status_updated)
-    self.analyser = parser.Analyser(self.knowledge)
-    self.monitor = parser.Monitor(self.knowledge, self.vehicle)
-    self.planner = control.Planner(self.knowledge)
-    self.executor = control.Executor(self.knowledge,self.vehicle)
-    self.prev_time = int(round(time.time() * 1000))
-    self.route_finished = lambda *_, **__: None
-    self.crashed = lambda *_, **__: None
+    def __init__(self, vehicle):
+        self.vehicle = vehicle
+        self.knowledge = data.Knowledge()
+        self.knowledge.set_status_changed_callback(self.status_updated)
+        self.analyser = parser.Analyser(self.knowledge)
+        self.monitor = parser.Monitor(self.knowledge, self.vehicle)
+        self.planner = control.Planner(self.knowledge, self.vehicle)
+        self.executor = control.Executor(self.knowledge, self.vehicle)
+        self.prev_time = int(round(time.time() * 1000))
+        self.route_finished = lambda *_, **__: None
+        self.crashed = lambda *_, **__: None
 
-  def status_updated(self, new_status):
-    if new_status == data.Status.ARRIVED:
-      self.route_finished(self)
-    if new_status == data.Status.CRASHED:
-      self.crashed(self)
+    def status_updated(self, new_status):
+        if new_status == data.Status.ARRIVED:
+            self.route_finished(self)
+        if new_status == data.Status.CRASHED:
+            self.crashed(self)
 
-  def set_route_finished_callback(self, callback):
-    self.route_finished = callback
+    def set_route_finished_callback(self, callback):
+        self.route_finished = callback
 
-  def set_crash_callback(self, callback):
-    self.crashed = callback
+    def set_crash_callback(self, callback):
+        self.crashed = callback
 
-  def get_vehicle(self):
-    return self.vehicle
- 
-  #Update all the modules and return the current status
-  def update(self):
-    ctime = int(round(time.time() * 1000))
-    delta_time = ctime - self.prev_time
-    self.prev_time = ctime
+    def get_vehicle(self):
+        return self.vehicle
 
-    self.monitor.update(delta_time)
-    self.analyser.update(delta_time)
-    self.planner.update(delta_time)
-    self.executor.update(delta_time)
+    # Update all the modules and return the current status
+    def update(self):
+        ctime = int(round(time.time() * 1000))
+        delta_time = ctime - self.prev_time
+        self.prev_time = ctime
 
-    return self.knowledge.get_status()
+        self.monitor.update(delta_time)
+        self.analyser.update(delta_time)
+        self.planner.update(delta_time)
+        self.executor.update(delta_time)
 
-  # Main interaction point with autopilot - set the destination, so that it does the rest
-  def set_destination(self, destination):
-    self.planner.make_plan(self.vehicle.get_transform(), destination)
+        return self.knowledge.get_status()
 
-  
-
-
-  
+    # Main interaction point with autopilot - set the destination, so that it does the rest
+    def set_destination(self, destination):
+        self.planner.make_plan(self.vehicle.get_transform(), destination)
