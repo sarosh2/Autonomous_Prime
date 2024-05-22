5af3764dacc90d86622f572dac6b22d5fb37b0ee
alexcega <alexcg24600@gmail.com>
2024-05-22 16:46:33 +0300

    facing direction function

--- ai_parser.py
+++ ai_parser.py
@@ -3,7 +3,6 @@
 import glob
 import os
 import sys
-
 try:
     sys.path.append(
         glob.glob(
@@ -22,113 +21,13 @@ import weakref
 import carla
 import ai_knowledge as data
 import numpy as np
+import math
 
 
 # Monitor is responsible for reading the data from the sensors and telling it to the knowledge
 # TODO: Implement other sensors (lidar and depth sensors mainly)
 # TODO: Use carla API to read whether car is at traffic lights and their status, update it into knowledge
 class Monitor(object):
-  def __init__(self, knowledge, vehicle):
-    self.vehicle = vehicle
-    self.knowledge = knowledge
-    weak_self = weakref.ref(self)
-    
-    self.knowledge.update_data('location', self.vehicle.get_transform().location)
-    self.knowledge.update_data('rotation', self.vehicle.get_transform().rotation)
-
-    world = self.vehicle.get_world()
-    bp = world.get_blueprint_library().find('sensor.other.lane_invasion')
-    self.lane_detector = world.spawn_actor(bp, carla.Transform(), attach_to=self.vehicle)
-    self.lane_detector.listen(lambda event: Monitor._on_invasion(weak_self, event))
-
-    #create LIDAR sensor
-    self.setup_lidar(world)
-
-    # Checking traffic light state
-    closest_tl = self.get_nearby_traffic_light(vehicle, world, 50000)
-    # TODO check below line code implementation
-    self.knowledge.update_data("traffic_light_value", self.get_traffic_light_state(closest_tl))
-    # self.knowledge.memory["traffic_light_value"] = self.get_traffic_light_state(closest_tl)
-
-    #create depth sensor
-
-  #convert lidar information into an np array and send it to knowledge
-  def lidar_callback(self, point_cloud):
-    data = np.copy(np.frombuffer(point_cloud.raw_data, dtype=np.dtype('f4')))
-    data = np.reshape(data, (int(data.shape[0] / 4), 4))
-    self.knowledge.update_data('lidar_data', data)
-
-
-  def setup_lidar(self, world):
-
-    #setup lidar blueprints and attributes
-    lidar_bp = world.get_blueprint_library().find('sensor.lidar.ray_cast')
-    lidar_bp.set_attribute('range',str(100))
-    lidar_bp.set_attribute('noise_stddev',str(0.1))
-    lidar_bp.set_attribute('upper_fov',str(15.0))
-    lidar_bp.set_attribute('lower_fov',str(-25.0))
-    lidar_bp.set_attribute('channels',str(64.0))
-    lidar_bp.set_attribute('points_per_second',str(500000))
-    lidar_bp.set_attribute('rotation_frequency',str(20.0))
-    lidar_transform = carla.Transform(carla.Location(z=2))
-
-    #create lidar sensor
-    self.lidar_sensor = world.spawn_actor(lidar_bp, lidar_transform, attach_to=self.vehicle)
-    self.lidar_sensor.listen(self.lidar_callback)
-
-  def get_nearby_traffic_light(self, vehicle, world, distance_threshold=50):
-    print("Check flag ai parser")
-    # Get the location and forward vector of the vehicle
-    vehicle_location = vehicle.get_location()
-    vehicle_transform = vehicle.get_transform()
-    vehicle_forward_vector = vehicle_transform.get_forward_vector()
-
-    # Get all traffic lights in the world
-    traffic_lights = world.get_actors().filter('traffic.traffic_light')
-    print("len of traffic lights", len(traffic_lights))
-    # Find the nearest traffic light within the distance threshold and in front of the vehicle
-    closest_traffic_light = None
-    min_distance = distance_threshold
-
-    for traffic_light in traffic_lights:
-        # Get the location of the traffic light
-        traffic_light_location = traffic_light.get_location()
-
-        # Calculate the distance from the vehicle to the traffic light
-        distance = vehicle_location.distance(traffic_light_location)
-
-        if distance < min_distance:
-            # Calculate the direction vector from the vehicle to the traffic light
-            direction_vector = traffic_light_location - vehicle_location
-            direction_vector = direction_vector.make_unit_vector()
-
-            # Calculate the dot product to check if the traffic light is in front of the vehicle
-            dot_product = direction_vector.x * vehicle_forward_vector.x + direction_vector.y * vehicle_forward_vector.y + direction_vector.z * vehicle_forward_vector.z
-
-            if dot_product > 0:  # Traffic light is in front of the vehicle
-                closest_traffic_light = traffic_light
-                min_distance = distance
-
-    return closest_traffic_light
-
-  def get_traffic_light_state(self, traffic_light):
-    if traffic_light:
-        print("checking state", traffic_light.get_state())
-        return traffic_light.get_state()
-    return None
-  
-  #Function that is called at time intervals to update ai-state
-  def update(self, time_elapsed):
-    # Update the position of vehicle into knowledge
-    self.knowledge.update_data('location', self.vehicle.get_transform().location)
-    self.knowledge.update_data('rotation', self.vehicle.get_transform().rotation)
-
-  @staticmethod
-  def _on_invasion(weak_self, event):
-    self = weak_self()
-    if not self:
-      return
-    self.knowledge.update_data('lane_invasion',event.crossed_lane_markings)
     def __init__(self, knowledge, vehicle):
         self.vehicle = vehicle
         self.knowledge = knowledge
@@ -155,6 +54,12 @@ class Monitor(object):
 
         # create LIDAR sensor
         self.setup_lidar(world)
+
+        # Checking traffic light state
+        closest_tl = self.get_nearby_traffic_light(vehicle, world, 50000)
+        # TODO check below line code implementation
+        self.knowledge.update_data("traffic_light_value", self.get_traffic_light_state(closest_tl))
+        # self.knowledge.memory["traffic_light_value"] = self.get_traffic_light_state(closest_tl)
 
         # create depth sensor
 
@@ -221,13 +126,44 @@ class Monitor(object):
                 if dot_product > 0:  # Traffic light is in front of the vehicle
                     closest_traffic_light = traffic_light
                     min_distance = distance
-
+        print("traffic light direction: ",self.get_facing_direction(closest_traffic_light))
+        print("vehicle direction: ", self.get_facing_direction(vehicle))
         return closest_traffic_light
 
     def get_traffic_light_state(self, traffic_light):
         if traffic_light:
             return traffic_light.get_state()
         return None
+
+    def get_facing_direction(self, actor):
+      # Get the transform of the actor (traffic light or vehicle)
+      transform = actor.get_transform()
+      
+      # Extract the rotation (pitch, yaw, roll)
+      rotation = transform.rotation
+      
+      # Calculate the facing direction based on the yaw angle
+      yaw = rotation.yaw
+      facing_direction = carla.Vector3D(
+          x=math.cos(math.radians(yaw)),
+          y=math.sin(math.radians(yaw)),
+          z=0
+      )
+      return facing_direction
+
+    def are_directions_aligned(self, direction1, direction2, threshold=0.95):
+        # Calculate the dot product of the two direction vectors
+        dot_product = (direction1.x * direction2.x + 
+                      direction1.y * direction2.y + 
+                      direction1.z * direction2.z)
+        
+        # Normalize the dot product (it should be between -1 and 1)
+        magnitude1 = math.sqrt(direction1.x**2 + direction1.y**2 + direction1.z**2)
+        magnitude2 = math.sqrt(direction2.x**2 + direction2.y**2 + direction2.z**2)
+        normalized_dot_product = dot_product / (magnitude1 * magnitude2)
+        
+        # Check if the directions are aligned within the given threshold
+        return normalized_dot_product > threshold
 
     # Function that is called at time intervals to update ai-state
     def update(self, time_elapsed):
@@ -289,6 +225,7 @@ class Analyser(object):
         is_vehicle = False
 
         for pdata in lidar_data:
+          
             if self.get_distance(pdata) < self.vehicle_threshold:
                 #clear
                 #print(" Checking for vehicle")
@@ -313,6 +250,5 @@ class Analyser(object):
         if self.knowledge.get_status() == data.Status.CRASHED:
             return
         self.analyse_lidar()
-        print("Traffic light value from Knowledge:", self.knowledge.get_closest_traffic_light_state())
         print("Lidar Data from Knowledge: ", self.knowledge.get_status())
         return
